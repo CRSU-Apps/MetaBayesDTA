c9d69f04d6ac6c97013a2810a582755806bea498
Ryan Field <RyanJField@users.noreply.github.com>
2023-08-31 13:08:56 +0100

    Add save if the model is compiled

--- R/model_stan.R
+++ R/model_stan.R
@@ -2,6 +2,7 @@ StanModel <- R6::R6Class(
     classname = "StanModel",
     private = list(
         stan_model_rds_path = "",
+        stan_model_rds_save_path = "",
         stan_model_path = "",
         stan_model_object = NULL
     ),
@@ -9,6 +10,9 @@ StanModel <- R6::R6Class(
         initialize = function(stan_model_rds_path = "", stan_model_path = ""){
             if(file.exists(stan_model_rds_path)){
                 private$stan_model_rds_path = stan_model_rds_path
+            }
+            else{
+                private$stan_model_rds_save_path = stan_model_rds_path
             }
             if(file.exists(stan_model_path)){
                 private$stan_model_path = stan_model_path
@@ -20,10 +24,22 @@ StanModel <- R6::R6Class(
             }
             if(!is.null(private$stan_model_rds_path) & private$stan_model_rds_path != ""){
                 private$stan_model_object <- readRDS(private$stan_model_rds_path)
+                # Run the Garabage Collector to Ensure any excess memory used by stan is freed (only if model is recompiled)
+                gc()
                 return(private$stan_model_object)
             }
             else{
                 private$stan_model_object <- stan_model(file = private$stan_model_path)
+                # Try to save the compiled model (this may fail if the directory is write protected)
+                if(!is.null(private$stan_model_rds_save_path) & private$stan_model_rds_save_path != ""){
+                    tryCatch({
+                       saveRDS(private$stan_model_object, private$stan_model_rds_save_path) 
+                    },
+                    error = function(e){
+                        print("Could not save compiled stan model, this model will recompile next run")
+                    })                    
+                }
+                # Run the Garabage Collector to Ensure any excess memory used by stan is freed
                 gc()
                 return(private$stan_model_object)
             }            
