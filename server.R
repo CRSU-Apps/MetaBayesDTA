eff2a9eda94732fe8354bab319f898df37d0ca52
tommorris168 <134509036+tommorris168@users.noreply.github.com>
2023-10-26 10:02:52 +0100

    Revert "Issues 25 to 29"

--- server.R
+++ server.R
@@ -17,7 +17,7 @@
 server <- function(input, output, session) {
   
   shinyalert(title = "Important message",
-             text =  paste0("If you have time it would be greatly appreciated if you could fill 
+             text =  paste0("Note that this is a beta release. If you have time it would be greatly appreciated if you could fill 
                             out the user feedback questionnaire ",
                             tags$a(href="https://docs.google.com/forms/d/e/1FAIpQLSdBvMFpWma87JV1R0lAkmiVWxcIFf9I0m2BiDS6JV20MrvE9Q/viewform?vc=0&c=0&w=1&flr=0"
                                    , "here.",target="_blank"),
@@ -30,7 +30,7 @@ server <- function(input, output, session) {
                text =  paste("In accordance with Data Protection legislation, we would like to inform you of the following before you
                         use our website: We collect your usage data within the MetaBayesDTA app to perform analytics of usage and
                         improve our app.  By clicking",   tags$i(tags$u("I consent")), "below, you consent to the use of data
-                        by us through Google Analytics.  For details of policy, please check",
+                        by us through Google Analytics.  For details of policy, please check 'Privacy notice' tab within the app, and",
                         tags$a(href="https://policies.google.com/privacy?hl=en", "Google Privacy & Terms.",target="_blank") 
                         ),
                type = "info",
@@ -70,18 +70,6 @@ output$downloadUG <- downloadHandler(
       }
   
 )
-
-#Download the instructions for reproducing analyses in the Cochrane Handbook
-output$downloadCochraneGuide <- downloadHandler(
-  filename = function(){
-    paste("Cochrane-DTA-in-MetaBayesDTA v1_0.html")
-  },
-  content = function(file){
-    file.copy("https://github.com/CRSU-Apps/MBDTA-Cochrane-guide/blob/main/Cochrane-DTA-in-MetaBayesDTA.html?raw=TRUE", file) 
-  }
-)
-
-
   
 # Load data tab ---------------------------------------------------------------  ---------------------------------------------------------------------------------------------------------------
 data_for_analysis_server(id = "dataset_id", 
