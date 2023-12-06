d1e847f8b9a173451b91013a23e0eb4e269d832f
Ryan J Field <RyanJField@users.noreply.github.com>
2023-12-06 15:41:11 +0000

    Add renv and deploy action

---
+++ compile_models.R
@@ -0,0 +1,15 @@
+stan_files <- list.files(path = "models", pattern = "stan")
+for (stan_file in stan_files){
+  f <- tools::file_path_sans_ext(stan_file)
+  fRDS <- paste0("models/", f, ".rds")
+  fstan <- paste0("models/", stan_file)
+  saveRDS(stan_model(file =fstan), fRDS)
+}
+
+stan_files <- list.files(path = "models/p_scale_priors/", pattern = "stan")
+for (stan_file in stan_files){
+  f <- tools::file_path_sans_ext(stan_file)
+  fRDS <- paste0("models/p_scale_priors/", f, ".rds")
+  fstan <- paste0("models/p_scale_priors/", stan_file)
+  saveRDS(stan_model(file =fstan), fRDS)
+}
