6608afa6d266cdc8e9d0701b48d05db0b752dbed
Ryan J Field <57794045+RyanJField@users.noreply.github.com>
2024-01-15 10:05:51 +0000

    Scope stan_models

--- compile_models.R
+++ compile_models.R
@@ -3,7 +3,7 @@ for (stan_file in stan_files){
   f <- tools::file_path_sans_ext(stan_file)
   fRDS <- paste0("models/", f, ".rds")
   fstan <- paste0("models/", stan_file)
-  saveRDS(stan_model(file =fstan), fRDS)
+  saveRDS(rstan::stan_model(file =fstan), fRDS)
 }
 
 stan_files <- list.files(path = "models/p_scale_priors/", pattern = "stan")
@@ -11,5 +11,5 @@ for (stan_file in stan_files){
   f <- tools::file_path_sans_ext(stan_file)
   fRDS <- paste0("models/p_scale_priors/", f, ".rds")
   fstan <- paste0("models/p_scale_priors/", stan_file)
-  saveRDS(stan_model(file =fstan), fRDS)
+  saveRDS(rstan::stan_model(file =fstan), fRDS)
 }
