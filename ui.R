94c08aea02c450cd66c85b9191128ccd3bf2e1bb
Tom Morris <tommorris168@gmail.com>
2024-02-21 09:00:31 +0000

    Formatted text.

--- ui.R
+++ ui.R
@@ -96,7 +96,18 @@ tabItem(
                             img(src = 'funded-by-nihr-logo.png', width = "55%")),
                         div(style = "display: inline;",
                             img(src = 'CRSU_logo.png', width = "40%")),
-                        div(tags$strong("Funding and Support Acknowledgement:"), tags$p("MetaBayesDTA is part of the Complex Reviews Synthesis Unit (CRSU) suite of evidence synthesis apps. The development of these apps is currently funded (majority) and overseen by the Evidence Synthesis Group @ CRSU (NIHR153934). The CRSU Evidence Synthesis Group is one of the groups funded by the National Institute for Health and Care Research ", tags$a(href="https://www.nihr.ac.uk/explore-nihr/funding-programmes/evidence-synthesis.htm", "(NIHR) Evidence Synthesis Programme."), "Further details of other funders and support, current and past, can be found ", tags$a(href="https://github.com/CRSU-Apps/.github/wiki/Detailed-Funding-Statement", "on our GitHub page"), ". The views expressed are those of the author(s) and not necessarily those of the NIHR or the Department of Health and Social Care."), tags$p(          "More information about the UK NIHR Complex Reviews Synthesis Unit (CRSU) can be found ", tags$a(href="https://www.gla.ac.uk/research/az/evidencesynthesis/apps-materials-guidence/", "on our website", target="_blank"),)
+                        div(tags$strong("Funding and Support Acknowledgement:"),
+                            tags$p("MetaBayesDTA is part of the Complex Reviews Synthesis Unit (CRSU) suite of evidence synthesis apps. The development of these apps is currently funded (majority) and overseen by the Evidence Synthesis Group @ CRSU (NIHR153934). The CRSU Evidence Synthesis Group is one of the groups funded by the National Institute for Health and Care Research ",
+                                   tags$a(href="https://www.nihr.ac.uk/explore-nihr/funding-programmes/evidence-synthesis.htm",
+                                          "(NIHR) Evidence Synthesis Programme."),
+                                   "Further details of other funders and support, current and past, can be found ",
+                                   tags$a(href="https://github.com/CRSU-Apps/.github/wiki/Detailed-Funding-Statement",
+                                          "on our GitHub page"),
+                                   ". The views expressed are those of the author(s) and not necessarily those of the NIHR or the Department of Health and Social Care."),
+                            tags$p("More information about the UK NIHR Complex Reviews Synthesis Unit (CRSU) can be found ",
+                                   tags$a(href="https://www.gla.ac.uk/research/az/evidencesynthesis/apps-materials-guidence/",
+                                          "on our website", target="_blank"),
+                                   )
                             )
                         ),
                       br(),
